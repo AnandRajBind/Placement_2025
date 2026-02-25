005c10e90078531407c5d01bd220c99cadb45218
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-02-25 12:19:23 +0530

    25/02/2026
     remove duplicate from the array by using Using Extra Array.

--- TCS/remove_duplicate.java
+++ TCS/remove_duplicate.java
@@ -9,24 +9,43 @@
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 // Note: HashSet does NOT maintain the original order of elements.
-
-
 import java.util.HashSet;
+
 public class remove_duplicate {
 
     public static void main(String[] args) {
 
-        int arr[] = {10, 20, 10, 30, 20, 40};
+        int arr[] = {10, 10,10, 20, 10, 30, 20, 40};
 
         // remove duplicate from the array by using hashSet.
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             set.add(arr[i]);
         }
-        System.out.println(set);
+        // System.out.println(set);
 
+        // remove duplicate from the array by using Using Extra Array.
+        int size = 0;
+        boolean found = false;
+        int result[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            found = false;
 
-    // remove duplicate from the array by using hashSet.
+            for (int j = 0; j < size; j++) {
+                if (result[j] == arr[i]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found == false) {
+                result[size] = arr[i];
+                size++;
+            }
+        }
+        // print array element after remove duplicate value.
+        for (int i = 0; i < size; i++) {
+            System.out.println(result[i]);
+        }
 
     }
 }
