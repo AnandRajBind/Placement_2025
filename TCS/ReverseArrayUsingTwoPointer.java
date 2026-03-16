1a6be9dc598a607e5a45a8a88dc634f7828b2e05
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-03-16 22:50:08 +0530

    16/03/2026
    Reverse Array using Two Pointer Techniques.

---
+++ TCS/ReverseArrayUsingTwoPointer.java
@@ -0,0 +1,22 @@
+
+public class ReverseArrayUsingTwoPointer {
+
+    public static void swap(int arr[], int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
