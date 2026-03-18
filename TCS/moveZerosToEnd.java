5541b0a212dc16ec7c442caea44bc8c73b5544ce
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-03-18 19:53:33 +0530

    18/03/2026
    Move zeros element to the last position in the array.

---
+++ TCS/moveZerosToEnd.java
@@ -0,0 +1,20 @@
+
+public class moveZerosToEnd {
+
+    public static void main(String[] args) {
+        int arr[] = {0, 10, 20, 3, 0, 0, 5, 15};
+        int slow = 0;
+        for (int fast = 0; fast < arr.length; fast++) {
+            if (arr[fast] != 0) {
+                arr[slow] = arr[fast];
+                slow++;
+            }
+        }
+        for (int i = slow; i <arr.length; i++) {
+                 arr[i]=0;
+         }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
