4d50bcb0af1c1941c302b3a02ba60dd2b3df8555
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-04-04 19:56:26 +0530

    04/04/2026
    count the vowels and consonants using the indexOf() Method.

--- stringProgram/count_Vowel_Consonents2.java
+++ stringProgram/count_Vowel_Consonents2.java
@@ -1,28 +1,27 @@
-//count the vowels and consonents using Multiple condition checking.
+
+//count the vowels and consonents using indexOf() method.
+
 import java.util.Scanner;
 
-public class Count_Vowel_Consonents {
+public class count_Vowel_Consonents2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String");
         String str = sc.nextLine().toLowerCase();
         int vowels = 0;
         int consonants = 0;
-
+        String VowelsStr = "aeiou";
         for (int i = 0; i < str.length(); i++) {
 
-            // char ch[] =str.toCharArray();
             char ch = str.charAt(i);
             if (Character.isLetter(ch)) {
-                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                if (VowelsStr.indexOf(ch) != -1) {
                     vowels++;
                 } else {
                     consonants++;
                 }
             }
-            System.out.println(Character.isLetter(ch));
         }
-        System.out.println("Vowels is: " + vowels + " Consonets is: " + consonants);
-        sc.close();
+        System.out.println("Vowels is: " + vowels + "Consonents is : " + consonants);
     }
 }
