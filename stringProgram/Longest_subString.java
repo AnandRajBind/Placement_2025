52e7390a36b411ccaf131d441b80f3ba00acbf6c
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-04-14 14:05:46 +0530

    14/04/2026

---
+++ stringProgram/Longest_subString.java
@@ -0,0 +1,12 @@
+// Longest Substring Without Repeating Characters.We use Sliding Window + Two Pointers + HashSet
+
+import java.util.HashSet;
+
+public class Longest_subString {
+    public static void main(String[] args) {
+        String str = "abcabcbb";
+        HashSet<Character> set=new HashSet<>();
+
+
+    }
+}
